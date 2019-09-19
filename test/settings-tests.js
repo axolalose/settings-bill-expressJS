7fc961058fb959713427ba259b24350babee3049
axolalose <axolalose@gmail.com>
2019-09-19 10:57:04 +0200

    new tests

--- test/settings-tests.js
+++ test/settings-tests.js
@@ -36,4 +36,60 @@ describe('settings-bill', function(){
             criticalLevel: 10
         });
     });
+   
 });
+describe('Warnings for Settings-Bill', function(){
+    it('should return warning when you have reached the limit of the warningLevel', function(){
+        const settingsBill = SettingsBill();
+
+        settingsBill.setSettings({
+            smsCost: 3.50,
+            callCost: 5.00,
+            warningLevel: 10,
+            criticalLevel:20,
+        })
+        settingsBill.recordAction('call');
+        settingsBill.recordAction('call');
+        settingsBill.recordAction('sms');
+
+        assert.equal(true, settingsBill.hasReachedWarningLevel());
+    });
+  
+
+    it('should return danger when it has reached the critical level', function(){
+        const settingsBill = SettingsBill();
+        settingsBill.setSettings({
+            smsCost: 2.50,
+            callCost: 5.00,
+            warningLevel: 5,
+            criticalLevel: 10
+        });
+
+        settingsBill.recordAction('call');
+        settingsBill.recordAction('call');
+        settingsBill.recordAction('sms');
+
+        assert.equal(true, settingsBill.hasReachedCriticalLevel());
+
+    });
+});
+describe('Actions for Settings-Bill', function(){
+    const settingsBill = SettingsBill();
+    settingsBill.setSettings({
+        smsCost: 2.35,
+        callCost: 3.35,
+        warningLevel: 30,
+        criticalLevel: 40
+    });
+
+    settingsBill.recordAction('call');
+    settingsBill.recordAction('call');
+    settingsBill.recordAction('sms');
+    settingsBill.recordAction('sms');
+
+    assert.equal(4.70, settingsBill.totals().smsTotal);
+    assert.equal(6.70, settingsBill.totals().callTotal);
+    assert.equal(11.40, settingsBill.totals().grandTotal);
+
+});
+
